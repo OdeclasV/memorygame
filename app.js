eeddbe6fd1323b550a591af578bf03fc1eacc361
Victor Salcedo <victorsalc2@gmail.com>
2017-11-24 20:42:20 -0500

    feat: compareChoices function Create compareChoices function that could serve as comparison between to user choices during the game. The function takes two arguments and outputs a string sayin if the cards matched or did not match

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// deck of cards
+// Array cards containting deck of cards and their illustrations
 
 var cards = [
 
@@ -19,3 +19,29 @@ function Shuffle(array) {
 	for(var j, x, i = array.length; i; j = parseInt(Math.random() * i), x = array[--i], array[i] = array[j], array[j] = x);
 	return array;
 };
+
+
+function compareChoices (cardNumber1, cardNumber2) {
+
+/* 
+ * Compares two user choices. 
+ * Takes two cards numbers are input. 
+ * Outputs a string noting if the cards match or don't match
+ */
+	var choice1 = cards[cardNumber1 - 1].draw;
+	var choice2 = cards[cardNumber2 - 1].draw;
+	
+	if (choice1 === choice2) {
+		return "They match!";
+	} 
+	else {
+		return "Cards don't match. Try again.";
+	}
+
+}
+
+
+// function choiceOne (card_number) {
+// 	var choice1 = cards[card_number - 1].draw; 
+// 	return choice1;
+// };
